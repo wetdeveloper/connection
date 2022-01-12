98cc6ad27bc175039d8e9b45dd97a45b939975b1
Ehsan Heydary <58146358+wetdeveloper@users.noreply.github.com>
2022-01-12 12:25:05 +0330

    Update connection.go

--- connection.go
+++ connection.go
@@ -11,12 +11,13 @@ import (
 
 func Connect() (db *sql.DB, err error) {
 	fmt.Println("connection.go is running")
-	//RemoveDb()
+	// RemoveDb()
 	// CreateDb()
 	sqliteDatabase, _ := sql.Open("sqlite3", "./users.db")
 	// defer sqliteDatabase.Close()
 	// CreateTable(sqliteDatabase)
-	// InsertUser(sqliteDatabase, "Ali", "5636532")
+	// InsertUser(sqliteDatabase, "Ehsan", "5636532")
+	// DeleteUser(sqliteDatabase, "Ehsan", "5636532")
 	// ListUsers(sqliteDatabase)
 	return sqliteDatabase, err
 }
@@ -25,10 +26,10 @@ func CreateTable(db *sql.DB) {
 		"idUser" integer NOT NULL PRIMARY KEY AUTOINCREMENT,		
 		"username" TEXT NOT NULL UNIQUE,
 		"password" TEXT
-	  );` // SQL Statement for Create Table
+	  );` 
 
 	log.Println("Create user table...")
-	statement, err := db.Prepare(createUsersTableSQL) // Prepare SQL Statement
+	statement, err := db.Prepare(createUsersTableSQL) 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -36,31 +37,79 @@ func CreateTable(db *sql.DB) {
 	log.Println("user table created")
 }
 
-func InsertUser(db *sql.DB, username string, password string) {
-	log.Println("Inserting user record ...")
-	insertUserSQL := `INSERT INTO users(username,password) VALUES (?, ?)`
-	statement, err := db.Prepare(insertUserSQL)
+func InsertUser(db *sql.DB, username string, password string) bool {
+	if SelectUser(db, username) == false {
+		log.Println("Inserting user record ...")
+		insertUserSQL := `INSERT INTO users(username,password) VALUES (?, ?)`
+		statement, err := db.Prepare(insertUserSQL)
 
-	if err != nil {
-		log.Fatalln(err.Error())
+		if err != nil {
+			log.Fatalln(err.Error())
+			return false
+		}
+		_, err = statement.Exec(username, password)
+		if err != nil {
+			log.Fatalln(err.Error())
+			return false
+		}
+		return true
 	}
-	_, err = statement.Exec(username, password)
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
+	return false
 }
 
-func UpdateUser(db *sql.DB, username string, password string) {
-	log.Println("Updating user record ...")
-	UpdateUserSQL := `UPDATE users SET password=? where username=?`
-	statement, err := db.Prepare(UpdateUserSQL)
+func UpdateUser(db *sql.DB, username string, password string) bool {
+	if SelectUser(db, username) {
+		log.Println("Updating user record ...")
+		UpdateUserSQL := `UPDATE users SET password=? where username=?`
+		statement, err := db.Prepare(UpdateUserSQL)
 
-	if err != nil {
-		log.Fatalln(err.Error())
+		if err != nil {
+			log.Fatalln(err.Error())
+			return false
+		}
+		_, err = statement.Exec(password, username)
+		if err != nil {
+			log.Fatalln(err.Error())
+			return false
+		}
+		return true
 	}
-	_, err = statement.Exec(password, username)
-	if err != nil {
-		log.Fatalln(err.Error())
+	return false
+}
+
+func DeleteUser(db *sql.DB, username string) bool {
+	if SelectUser(db, username) {
+		log.Println("Deleting user record ...")
+		UpdateUserSQL := `DELETE FROM  users WHERE username=?`
+		statement, err := db.Prepare(UpdateUserSQL)
+
+		if err != nil {
+			log.Fatalln(err.Error())
+			return false
+		}
+		_, err = statement.Exec(username)
+		if err != nil {
+			log.Fatalln(err.Error())
+			return false
+		}
+		return true
+	}
+	return false
+}
+
+func SelectUser(db *sql.DB, Username string) bool {
+	sqlStatement := `SELECT username FROM users WHERE username=$1`
+	var username string
+	row := db.QueryRow(sqlStatement, Username)
+	switch err := row.Scan(&username); err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows were returned!")
+		return false
+	case nil:
+		fmt.Println(username)
+		return true
+	default:
+		panic(err)
 	}
 }
 
